17b47ed438ae6ea0922c332969b7deebbba9420d
Kerem-Tasan <128915083+Kerem-Tasan@users.noreply.github.com>
2023-03-31 00:58:04 +0300

    Add files via upload

---
+++ kombinasyon.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class kombinasyon {
+    public static void main(String[] args) {
+        Scanner verial = new Scanner(System.in);
+
+        System.out.println("Kümenin eleman sayısını giriniz");
+        int sayi = verial.nextInt();
+        System.out.println("Kaç farklı gurup bulunduğunu yazın");
+        
+        int gurup = verial.nextInt();
+        
+        int  toplam1 = 1, toplam2 = 1;
+        double sonuc = 0;
+        if (sayi > gurup) {
+            for (int i = 1; i <= sayi; ++i) {
+                toplam1 *= i;
+            }
+
+            for (int j = 1; j <= gurup; ++j) {
+                toplam2 *= j;
+
+            }
+            sonuc = toplam1 / (toplam2 * (sayi - gurup));
+
+        } else {
+            System.out.println("küme guruptan büyük olmalı.");
+        }
+        System.out.println(sonuc);
+
+    }
+}
